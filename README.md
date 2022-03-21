cc39cc21abeb9f1903edfa24b72235a1733cfaef
happylie <happylie@hanmail.net>
2022-03-22 01:29:33 +0900

    strip Update

--- README.md
+++ README.md
@@ -8,3 +8,7 @@
 **2. [Python] 파이썬 IF문 한줄로 표현하기(if, if-else, if-elif-else : 삼항 연산자)**
 + Directory : if_one-line
 + Link : https://happylie.tistory.com/115
+
+**3. [Python] 파이썬 문자열 특수문자, 공백 제거 하기(strip, rstrip, lstrip)**
++ Directory : strip 
++ Link : https://happylie.tistory.com/116
---
+++ strip/ex_strip.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# 1. 공백 제거
+test_text = ' Hello~~ Every One!!! '
+print("기본 텍스트: [" + test_text + "]")
+print("----------------------------------")
+print("양쪽 공백 제거: [" + test_text.strip() + "]")
+print("오른쪽 공백 제거: [" + test_text.rstrip() + "]")
+print("왼 공백 제거: [" + test_text.lstrip() + "]")
+print("\n\n")
+
+# 2. 특수문자 제거
+test_text2 = '@@@@Hello~~ Every One!!!@@@@'
+print("기본 텍스트: [" + test_text2 + "]")
+print("----------------------------------")
+print("양쪽 특수문자 제거: [" + test_text2.strip('@') + "]")
+print("오른쪽 특수문자 제거: [" + test_text2.rstrip('@') + "]")
+print("왼쪽 특수문자 제거: [" + test_text2.lstrip('@') + "]")
+print("\n\n")
+
+test_text3 = '@@ @@Hello~~ Every One!!!@@ @@'
+print("기본 텍스트: [" + test_text3 + "]")
+print("----------------------------------")
+print("양쪽 특수문자 제거: [" + test_text3.strip('@') + "]")
+print("오른쪽 특수문자 제거: [" + test_text3.rstrip('@') + "]")
+print("왼쪽 특수문자 제거: [" + test_text3.lstrip('@') + "]")
